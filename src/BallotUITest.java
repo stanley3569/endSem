44ef7a517d9c351eae984865b21bf7b6718fb376
stanley3569 <stanleyflofer@gmail.com>
2017-06-08 16:56:53 +0530

    4th

---
+++ src/BallotUITest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BallotUITest {
+
+	
+
+	@Test
+	public void test() {
+		BallotUI b1 =new BallotUI();
+		//fail("Not yet implemented");
+	}
+
+}
